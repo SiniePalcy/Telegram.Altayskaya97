843d3a78be597b6005af30aa246344f298b6b8fd
SiniePalcy <tg.altayski@gmail.com>
2020-10-13 23:11:05 +0300

    to prev commit

--- Bot/Bot.cs
+++ Bot/Bot.cs
@@ -72,7 +72,7 @@ namespace Telegram.Altayskaya97.Bot
             PeriodChatListMin = ParseInt(configSection.GetSection("PeriodChatListMin").Value, PERIOD_CHAT_LIST_MIN_DEFAULT);
 
             string botName = GlobalEnvironment.BotName.StartsWith("@") ? GlobalEnvironment.BotName.Remove(0, 1) : GlobalEnvironment.BotName;
-            string accessToken = configSection.GetSection(GlobalEnvironment.BotName).Value;
+            string accessToken = configSection.GetSection(botName).Value;
             _botClient = new TelegramBotClient(accessToken);
 
             _botClient.OnMessage += Bot_OnMessage;
